00b033caf7b1be01ec6b1e83d91e836f9415b503
michael_stanin <michael.stanin@gmail.com>
2019-03-24 14:21:11 +0200

    Introduce simple binary search algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Array.hpp"
+#include <functional>
 #include <iostream>
 #include <cassert>
 #include <vector>
@@ -99,11 +100,12 @@ namespace Module3
 	using std::cin;
 	using std::cout;
 	using std::endl;
+	using std::function;
 	using std::vector;
 
 	constexpr int kNotFound = -1;
 
-	int Search(int element, const int* arr, int size)
+	int LinearSearch(int element, const int* arr, int size)
 	{
 		// For each item in the input array,
 		// compare it with the search element
@@ -118,6 +120,31 @@ namespace Module3
 		return kNotFound;
 	}
 
+	int BinarySearch(int element, const int* arr, int size)
+	{
+		int left = 0;
+		int right = size - 1;
+
+		while (left <= right) {
+			int middle = (left + right) / 2;
+			
+			if (arr[middle] == element) {
+				// Element found
+				return middle;
+			}
+			else if (arr[middle] < element) {
+				// Search in the right-half: (middle+1..right)
+				left = middle + 1;
+			}
+			else if (arr[middle] > element) {
+				// Search in the left-half: (left..middle-1)
+				right = middle - 1;
+			}
+		}
+
+		return kNotFound; // ELement not found
+	}
+
 	void Print(const vector<int>& v)
 	{
 		cout << "[ ";
@@ -127,9 +154,8 @@ namespace Module3
 		cout << ']' << endl;
 	}
 	
-	void TestLinearSearch()
+	void TestSearch(const vector<int>& v, function<int(int, const int*, int)>&& search)
 	{
-		vector<int> v{ 33, 44, 55, 11, 22 };
 		Print(v);
 
 		cout << "Element to search? ";
@@ -138,7 +164,7 @@ namespace Module3
 
 		cout << endl;
 
-		int pos = Search(x, v.data(), v.size());
+		int pos = search(x, v.data(), v.size());
 		if (pos == kNotFound) {
 			cout << "Element not found." << endl;
 		}
@@ -146,6 +172,19 @@ namespace Module3
 			cout << "Element found at index " << pos << '.' << endl;
 		}
 	}
+
+	void TestLinearSearch()
+	{
+		vector<int> v{ 33, 44, 55, 11, 22 };
+		TestSearch(v, LinearSearch);
+	}
+
+	void TestBinarySearch()
+	{
+		vector<int> v{ 11, 22, 33, 44, 55 };
+		TestSearch(v, BinarySearch);
+	}
+
 }
 
 
@@ -156,6 +195,7 @@ int main()
 	//Module1::BoundsChecking();
 	//Module2::ConvinientlyPrintArrays();
 	//Module2::SubtleBugWhenCopyingArray();
-	Module3::TestLinearSearch();
+	//Module3::TestLinearSearch();
+	Module3::TestBinarySearch();
 	system("pause");
 }
